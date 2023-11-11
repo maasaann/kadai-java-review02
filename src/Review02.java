d8dab3923a140c2bd759ead412227b943d94ba99
yoshitoshi.​miyata <meyutana@gmail.com>
2023-11-11 10:15:45 +0900

    完成

--- src/Review02.java
+++ src/Review02.java
@@ -2,8 +2,21 @@
 public class Review02 {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
 
+        for (int i = 1; i <= 100; i++) {
+
+            String i_str = Integer.valueOf(i).toString();
+
+                   if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+
+        }
     }
-
 }
